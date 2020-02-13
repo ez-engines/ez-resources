c58cccaeeef5f17ffda7c5621d2ab20b193e2497
Andrew Shyika <andrew.shyika@gmail.com>
2020-02-13 13:44:00 +0200

    [SEARCH] Add field search through association

--- app/cells/ez/resources/search_cell.rb
+++ app/cells/ez/resources/search_cell.rb
@@ -3,8 +3,26 @@ module Ez
     class SearchCell < ApplicationCell
       form
 
+      def field_name(field)
+        if association?(field)
+          search_key(field.name, field.search_suffix, field.options.fetch(:association))
+        else
+          search_key(field.name, field.search_suffix)
+        end
+      end
+
       def field_label(field)
         "#{field.title} (#{t "search.suffix.#{field.search_suffix}"})"
+      end
+
+      private
+
+      def search_key(name, search_suffix, association_prefix = nil)
+        [association_prefix, name, search_suffix].compact.join("_").to_sym
+      end
+
+      def association?(field)
+        field.type == :association && field.options[:association].present?
       end
     end
   end
